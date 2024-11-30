11e0072783fc3eb15a4d2e9f6a6feb3a6ae3d9cd
Rose Enos <enosb@uci.edu>
2024-11-30 13:47:54 -0800

    Create header/footer, index, stylesheet

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>
+		<?php echo $title ?> - Roseeewater
+	</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+	<header>
+		<h1><a href="index.php">ROSEEEWATER</a></h1>
+		<nav>
+			<a href="index.php">Home</a>
+		</nav>
+	</header>
+	<article>
