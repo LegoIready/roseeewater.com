0f2e52b173518fe5f27050f5d862ada06b4c631f
Rose Enos <enosb@uci.edu>
2024-12-02 23:40:23 -0800

    Link to Vercel

--- api/header.php
+++ api/header.php
@@ -6,15 +6,15 @@
             <?php echo $title ?> - Roseeewater
         </title>
         <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="public/styles/styles.css">
+        <link rel="stylesheet" type="text/css" href="../public/styles/styles.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
 
     <body>
         <header>
-            <h1><a href="index.php">ROSEEEWATER</a></h1>
+            <h1><a href="">ROSEEEWATER</a></h1>
             <nav>
-                <a href="index.php">Home</a>
+                <a href="">Home</a>
             </nav>
         </header>
         <article>
