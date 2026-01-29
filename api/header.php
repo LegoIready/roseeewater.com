d4de831df56825cc486299a8d2431789a6b9b6d8
Rose Enos <enosb@uci.edu>
2026-01-28 23:22:01 -0800

    Styles path

--- api/header.php
+++ api/header.php
@@ -6,7 +6,7 @@
             <?php echo $title ?> - Roseeewater
         </title>
         <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="public/styles/styles.css">
+        <link rel="stylesheet" type="text/css" href="styles/styles.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
 
