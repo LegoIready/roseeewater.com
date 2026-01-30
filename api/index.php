5146565854f2f12ac69d4c85c789d35943635f4b
Rose Enos <enosb@uci.edu>
2026-01-29 22:28:41 -0800

    Musescore link

--- api/index.php
+++ api/index.php
@@ -6,7 +6,7 @@ include("header.php");
 <p>I read romance, queer theory, gender studies, and autotheory. I write realist fiction, critical art analysis, and positive psychology. My favorite literary works are <i>Testo Junkie</i>, <i>Fierce Femmes and Notorious Liars</i>, and <a href=https://archiveofourown.org/users/Roseeewater/bookmarks target="_blank">various web works</a>.</p>
 <p>I make short games and visual novels. I mostly play short, narrative-driven games, visual novels, and mystery games. My favorite games include <i>Minecraft</i>, <i>Ace Attorney</i>, <i>Danganronpa</i>, and <i>it gets so lonely here</i>. I'm also a fan of <i>Honkai: Star Rail</i>, <i>Love and Deepspace</i>, and <i>BTS World</i>.</p>
 <p>I like watching reality TV and K-dramas with my girlfriend, and I make mostly <a href="https://www.youtube.com/@roseeewater" target="_blank">let's-play videos</a>, also <a href="https://www.youtube.com/@tea-roses" target="_blank">with my girlfriend</a>. Some of my favorite film/TV works are <i>Everything, Everywhere, All at Once</i>, <i>I Saw the TV Glow</i>, <i>Star Wars</i>, <i>Single's Inferno</i>, and <a href="https://www.youtube.com/watch?v=RTRT794IQBg" target="_blank"><i>The Hunger</i></a>.</p>
-<p>I listen to The Beatles and TWICE, among other artists. I compose for my own games and sometimes transcribe songs I like.</p>
+<p>I listen to The Beatles and TWICE, among other artists. I <a href="https://musescore.com/user/31948198" target="_blank">compose</a> for my own games and sometimes transcribe songs I like.</p>
 <?php
 include("footer.php");
 ?>
