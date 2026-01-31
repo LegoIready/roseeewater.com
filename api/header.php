1294dde463c7f9f1cbe758bca29dd82e75e9372b
Rose Enos <enosb@uci.edu>
2026-01-31 14:36:09 -0800

    Add projects page

--- api/header.php
+++ api/header.php
@@ -14,7 +14,7 @@
         <header>
             <h1><a href="">ROSEEEWATER</a></h1>
             <nav>
-                <a href="/">Home</a><a href="/education">Education</a><a href="/publications">Publications</a>
+                <a href="/">Home</a><a href="/education">Education</a><a href="/publications">Publications</a><a href="/projects">Projects</a>
             </nav>
         </header>
         <article>
---
+++ api/projects.php
@@ -0,0 +1,27 @@
+<?php
+$title = "Projects";
+include("header.php");
+?>
+<h1>Projects</h1>
+<h2>Games</h2>
+<p>For game jams or personal projects, I have developed the following games:</p>
+<ul>
+    <li><a href="https://lego-iready.itch.io/buck-life" target="_blank">Buck Life.</a> ZotJam, 2026.</li>
+    <li><u>Rose Enos</u> and Julia Nguyen. <a href="https://lego-iready.itch.io/uc-cursed-love" target="_blank">UC Cursed Love.</a> ZotJam, 2025.</li>
+    <li><u>Rose Enos</u>, Julia Nguyen, and Cynthia Roxanne Ortiz. <a href="https://github.com/jploia/ucibogosort" target="_blank">Garbagotchi.</a> IrvineHacks, 2025.</li>
+    <li><a href="https://lego-iready.itch.io/nest-of-life" target="_blank">Nest of Life.</a> ZotJam, 2024.</li>
+    <li><u>Rose Enos</u>, Jaden Kim, Jason Nguyen, and Cynthia Roxanne Ortiz. <a href="https://uciseek.roseeewater.com" target="_blank">UCISeek.</a> ZotHacks, 2024.</li>
+    <li><a href="https://github.roseeewater.com/licatanai/licatanai.html" target="_blank">Settlers of Catan Companion.</a> 2020.</li>
+    <li><a href="https://github.roseeewater.com/licatanai/ms.html" target="_blank">Minesweeper.</a> 2020.</li>
+    <li><a href="https://github.roseeewater.com/marner/fnaf1.html" target="_blank">Five Nights at Freddy's.</a> 2015.</li>
+</ul>
+<p>As part of the quarterly project teams of the Video Game Developer Club at UCI, I have contributed to these:</p>
+<ul>
+    <li>Julia Nguyen, Karina C., and Lucas Z. <a href="https://jploia.itch.io/pass-on" target="_blank">Pass On.</a> Written by Roxanne Bandy, <u>Rose Enos</u>, and Patrick Wang, 2025.</li>
+    <li>Mehran Hossain and Diego Najera. <a href="https://fortunemh.itch.io/typing-gaem" target="_blank">Typing Gaem.</a> Programmed by Lexi Arballo, Derick Barrientos, Carson Beye, Marco Camacho, Tyler Cox, Ivan Guerrero Andres, and <u>Rose Enos</u>, 2024.</li>
+    <li>Ziqing Li. <a href="https://felbiter.itch.io/heartstone-academy" target="_blank">Heartstone Academy.</a> Written by <u>Rose Enos</u>, kirian490, and Shreya Pujari, 2024.</li>
+    <li>Ben Zhang. <a href="https://geminibllllue.itch.io/diabolic-lord-tdlf" target="_blank">Diabolic Lord.</a> Written by <u>Rose Enos</u>, 2024.</li>
+</ul>
+<?php
+include("footer.php");
+?>
