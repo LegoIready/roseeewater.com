deb45eb2aaea7df9bfac4a4bd8b525c481a542b3
Rose Enos <enosb@uci.edu>
2026-01-31 14:35:17 -0800

    Add languages to education

--- api/education.php
+++ api/education.php
@@ -26,9 +26,9 @@ include("header.php");
 </ul>
 <p>My primary interest is computer science education. Since June of 2025, I do research, advised by Professor Michael Shindler, about active learning, student experiences, and diversity, equity, and inclusion. I frequently collaborate with Julia Nguyen. I apply my knowledge as a tutor for the School of ICS, as the head learning assistant for the discrete math series, and as a peer. I intend to continue in computer science education as a PhD student.</p>
 <p>In the area of math, I am most interested in algebra and discrete math. I have completed the undergraduate series in calculus and algebra, and I am currently taking the linear algebra and analysis series. I look forward to studying topology, geometry, and number theory.</p>
-<p>In the area of computer science, I am interested in operating systems, theory of computation and (FOSS) programming. I have worked in C, C++, Java, JavaScript, PHP, Python (Numpy, Pyplot, Scikit-learn), and Matlab.</p>
+<p>In the area of computer science, I am interested in operating systems, theory of computation and (FOSS) programming. I have worked in Assembly, C, C++, Java, HTML, CSS, JavaScript, PHP, Python (Numpy, Pyplot, Scikit-learn), and Matlab.</p>
 <p>I participated in speech and debate from 5th to 12th grade. In high school, I focused on policy effects and issues linked to capitalism, with Leo Hsu. I was president of the debate club for two years and participated in national-circuit policy tournaments with the Sacramento Urban Debate League.</p>
-<p>In my free time, I study physics, epistemology, critical queer theory, and moral psychology, and I play the piano.</p>
+<p>I learned Spanish in high school and am currently learning Vietnamese. In my free time, I study physics, epistemology, critical queer theory, and moral psychology, and I play the piano.</p>
 <?php
 include("footer.php");
 ?>
