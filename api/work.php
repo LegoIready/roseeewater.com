2b7709f00860f0870e018bcfce17cf7c868510fb
Rose Enos <enosb@uci.edu>
2026-04-18 20:27:36 -0700

    SSM PDF

--- api/work.php
+++ api/work.php
@@ -9,6 +9,7 @@ include("header.php");
 <p>Since Summer 2025, I am an undergraduate research assistant for Professor Michael Shindler at UCI, with some time volunteered and some time compensated with academic credits, with a team of about 15 people, including graduate students and undergraduate students. I am responsible, with Julia Nguyen, for leading a study on student experiences and persistence. I also contribute to studies on active learning.</p>
 <p>In Spring 2024, I was a learning assistant for boolean logic and discrete structures taught by Professor Irene Gassko at UCI, with time compensated with academic credits, with a team of about 20 people, including graduate student TAs, undergraduate graders, and undergraduate learning assistants. In Winter 2025 and Winter 2026, I was the lead learning assistant for discrete mathematics taught by Professor Gassko, with teams of similar size but without graders in 2026. For these courses, I was responsible for supporting around 300 students each term in flipped-classroom activities, hosting weekly study hall sessions, meeting weekly to discuss the course content and structure, and proctoring exams. As a lead, I hosted some of our weekly meetings.</p>
 <h2>Volunteering</h2>
+<p>I was a general volunteer for <a href="https://www.sonandosinmiedo.org" target="_blank">Soñando Sin Miedo</a>'s first Professional Development Fair.</p>
 <p>I am a moderator for the following Discord servers:</p>
 <ul>
     <li>UCI Mathematics</li>
