6de880f7ea1375a8e8c3a36a0748022513fa7d48
Rose Enos <enosb@uci.edu>
2026-04-20 22:03:00 -0700

    Normalize relative links

--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
 $title = "Home";
 include("header.php");
 ?>
-<img src="images/rose.png" alt="Selfie by Rose Enos at a temple">
+<img src="/images/rose.png" alt="Selfie by Rose Enos at a temple">
 <p>Hello! I'm Rose Enos (she/they), a student at UC Irvine, where I study pure mathematics and intelligent computer systems. I do research on student experiences and diversity, equity, and inclusion in computer science. I also work as a tutor for the School of Information and Computer Sciences and as a learning assistant for the discrete math series. My curriculum vitae is <a href="/files/CV.pdf" target="_blank">here</a>.</p>
 <p>I read romance, queer theory, gender studies, and autotheory. I write realist fiction, critical art analysis, and positive psychology. My favorite literary works are <i>Testo Junkie</i>, <i>Fierce Femmes and Notorious Liars</i>, and <a href=https://archiveofourown.org/users/Roseeewater/bookmarks target="_blank">various web works</a>.</p>
 <p>I make short games and visual novels. I mostly play short, narrative-driven games, visual novels, and mystery games. My favorite games include <i>Minecraft</i>, <i>Ace Attorney</i>, <i>Danganronpa</i>, and <i>it gets so lonely here</i>. I'm also a fan of <i>Honkai: Star Rail</i>, <i>Love and Deepspace</i>, and <i>BTS World</i>.</p>
