d5884f518a0e1727f154ecbd3ba4089357c3eb5f
Rose Enos <enosb@uci.edu>
2026-04-24 15:01:05 -0700

    Add teaching

--- api/index.php
+++ api/index.php
@@ -8,6 +8,9 @@ include("header.php");
 <p>I make short games and visual novels. I mostly play short, narrative-driven games, visual novels, and mystery games. My favorite games include <i>Minecraft</i>, <i>Ace Attorney</i>, <i>Danganronpa</i>, and <i>it gets so lonely here</i>. I'm also a fan of <i>Honkai: Star Rail</i>, <i>Love and Deepspace</i>, and <i>BTS World</i>.</p>
 <p>I like watching reality TV and K-dramas with <a href="https://tiennguyenarts.wuaze.com" target="_blank">my girlfriend</a>, and I make mostly <a href="https://www.youtube.com/@roseeewater" target="_blank">let's-play videos</a>, also <a href="https://www.youtube.com/@tea-roses" target="_blank">with my girlfriend</a>. Some of my favorite film/TV works are <i>Everything, Everywhere, All at Once</i>, <i>I Saw the TV Glow</i>, <i>Star Wars</i>, <i>Single's Inferno</i>, and <a href="https://www.youtube.com/watch?v=RTRT794IQBg" target="_blank"><i>The Hunger</i></a>.</p>
 <p>I listen to The Beatles and TWICE, among other artists. I <a href="https://musescore.com/user/31948198" target="_blank">compose</a> for my own games and sometimes transcribe songs I like.</p>
+<h2>Teaching</h2>
+<p>For Spring 2026, I am a learning assistant for COMPSCI 162: Formal Languages and Automata. My office hours are 3–5pm on Monday in ICS 458C, with Julia Nguyen.</p>
+<p>For the 2025–2026 school year, I am a drop-in tutor at the ICS Tutoring Center in ICS2 110. I support ICS 6B, 6D, 31, 32, H32, 33, 45C, 46, IN4MATX 43, and STATS 67.</p>
 <?php
 include("footer.php");
 ?>
