74b6dc2ad31038ef0faf60c3120a2d881e01f1a7
Rose Enos <enosb@uci.edu>
2026-05-12 23:20:01 -0700

    Add GitHub and Discord

--- api/footer.php
+++ api/footer.php
@@ -1,6 +1,6 @@
         </article>
         <footer>
-            <p class="footer">&copy; 2026 Rose Enos</p>
+            <p class="footer">&copy; 2026 Rose Enos | <?php mention("https://github.com/LegoIready/roseeewater.com","Source"); ?> | <?php mention("https://discord.gg/KyXdtrPnWN","Discord"); ?></p>
         </footer>
     </body>
 
