43b144ee9eca66cabcd00ccfc9580f952300dd65
Rose Enos <enosb@uci.edu>
2026-05-26 16:53:41 -0700

    Add geometry notes

--- api/notes.php
+++ api/notes.php
@@ -6,6 +6,7 @@ include("header.php");
 <p>I read a lot of textbooks, and I take notes on them. These are meant to be refreshers or references, not full notes, so you won't find many explanations, proofs, or practice problems. Sources and upcoming notes are (soon to be) on my <?php mention("https://github.com/LegoIready","GitHub"); ?>.</p>
 <h2>Mathematics</h2>
 <ul>
+    <li><?php mention("/files/notes/Rose's Modern Geometry.pdf","Rose's Modern Geometry"); ?>. 2026.</li>
     <li><a href="/files/notes/Rose's Linear Algebra.pdf" target="_blank">"Rose's Linear Algebra."</a> 2026.</li>
     <li><a href="/files/notes/Rose's Statistics.pdf" target="_blank">"Rose's Statistics."</a> 2025.</li>
     <li><a href="/files/notes/Rose's Discrete Mathematics.pdf" target="_blank">"Rose's Discrete Mathematics."</a> 2025.</li>
