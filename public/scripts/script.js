7d9b818772e728567196ba957c7ac41c4b019667
Rose Enos <enosb@uci.edu>
2026-06-07 00:48:53 -0700

    declare sites in prior script tag?

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,5 @@
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const myself = "Rose Enos";
-let sites = {};
 
 function echo_list(section_id) {
     fetch("/files/" + section_id + "/" + section_id + ".json")
