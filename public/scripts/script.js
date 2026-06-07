4b96e519330b3e36fa1b8eb6ef7fbce350cc93c9
Rose Enos <enosb@uci.edu>
2026-06-07 00:05:02 -0700

    syntax

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -42,7 +42,7 @@ function generate_list(section_id, data) {
         if ("external" in item)
             str += " (<a href=\"" + item.external + "\" target=\"_blank\">external</a>)";
         if ("comments" in item)
-            str += "<ul><li>" + comments.join("</li><li>") + "</li></ul>";
+            str += "<ul><li>" + item.comments.join("</li><li>") + "</li></ul>";
         str += "</li>";
     }
     str += "</ul>";
