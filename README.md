322a6be2251e38efcf8d2ccc1126840f96bd4115
Rose Enos <enosb@uci.edu>
2026-06-08 23:56:31 -0700

    test authorname key format

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 My personal website written in PHP and hosted on Vercel.
 
 Available at [https://www.roseeewater.com](https://www.roseeewater.com).
+
+TODO:
+
+* Convert mention keys to "Authorname" format instead of "firstname"
--- api/functions.php
+++ api/functions.php
@@ -3,7 +3,9 @@ if(!function_exists("mention")) {
     $sites = array(
         "tien" => "https://tiennguyenarts.wuaze.com/?i=1",
         "julia" => "https://jploia.github.io/portfolio-2025/",
-        "michael" => "https://ics.uci.edu/~mikes/"
+        "michael" => "https://ics.uci.edu/~mikes/",
+        // eventually we want all of the keys to be formatted like below!
+        "Julia Nguyen" => 'https://jploia.github.io/portfolio-2025/'
     );
     function mention($what, $how) {
         echo "<a href=\"" . $what . "\" target=\"_blank\">" . $how . "</a>";
