31d1cdc42b910ea932c5ee1ed59fab5c22182f6b
Rose Enos <enosb@uci.edu>
2026-06-09 23:21:36 -0700

    og:image

--- api/header.php
+++ api/header.php
@@ -10,7 +10,7 @@
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="/styles/styles.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="image" content="/images/rocky.ico">
+        <meta name="og:image" content="/images/rocky.ico">
         <?php echo (isset($description) ? "<meta name=\"description\" content=\"$description\">" : ''); ?>
     </head>
 
