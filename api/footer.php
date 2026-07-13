15745c390a68a57f2593bc74e6dd9b4508e6b31f
Rose Enos <enosb@uci.edu>
2026-07-12 19:41:05 -0700

    email in footer

--- api/footer.php
+++ api/footer.php
@@ -1,6 +1,6 @@
         </article>
         <footer>
-            <p class="footer">&copy; 2026 Rose Enos | <?php mention("https://github.com/LegoIready/roseeewater.com","Source"); ?><!-- | <?php mention("https://discord.gg/KyXdtrPnWN","Discord"); ?>--> | <?php mention("https://www.linkedin.com/in/rose-enos","LinkedIn"); ?></p>
+            <p class="footer">&copy; 2026 Rose Enos | <?php mention('mailto:enosb@uci.edu','enosb@uci.edu'); ?> | <?php mention("https://github.com/LegoIready/roseeewater.com","GitHub"); ?><!-- | <?php mention("https://discord.gg/KyXdtrPnWN","Discord"); ?>--> | <?php mention("https://www.linkedin.com/in/rose-enos","LinkedIn"); ?></p>
         </footer>
     </body>
 
