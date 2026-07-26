95ca29b900719e99dd2b2fe76b6ea828446c007d
Rose Enos <enosb@uci.edu>
2026-07-26 16:19:05 -0700

    demote notes headers

--- api/resources.php
+++ api/resources.php
@@ -5,14 +5,14 @@ include('header.php');
 <h1>Resources</h1>
 <h2>Rosa's Notas</h2>
 <p>I read a lot of textbooks, and I take notes on them. These are meant to be refreshers or references, not full notes, so you won't find many explanations, proofs, or practice problems. Sources and upcoming notes are (soon to be) on my <?php mention("https://github.com/LegoIready","GitHub"); ?>.</p>
-<h2>Mathematics</h2>
+<p>Mathematics:</p>
 <ul>
     <li><?php mention("/files/notes/Rose's Modern Geometry.pdf","Rose's Modern Geometry"); ?>. 2026.</li>
     <li><a href="/files/notes/Rose's Linear Algebra.pdf" target="_blank">"Rose's Linear Algebra."</a> 2026.</li>
     <li><a href="/files/notes/Rose's Statistics.pdf" target="_blank">"Rose's Statistics."</a> 2025.</li>
     <li><a href="/files/notes/Rose's Discrete Mathematics.pdf" target="_blank">"Rose's Discrete Mathematics."</a> 2025.</li>
 </ul>
-<h2>Computer Science</h2>
+<p>Computer Science:</p>
 <ul>
     <li><a href="/files/notes/Rose's Programming in MATLAB.pdf" target="_blank">"Rose's Programming in MATLAB."</a> 2025.</li>
     <li><a href="/files/notes/Rose's Programming in Python.pdf" target="_blank">"Rose's Programming in Python."</a> 2024.</li>
