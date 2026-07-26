26e0a3cc2a36fda8ec91718653ceb531ef1bb22e
Rose Enos <enosb@uci.edu>
2026-07-26 16:08:24 -0700

    add resources page

---
+++ api/resources.php
@@ -0,0 +1,12 @@
+<?php
+$title = 'Resources';
+include('header.php');
+?>
+<h1>Resources</h1>
+<p>These are some helpful resources written by other people.</p>
+<section id="resources">
+    <script> echo_list("resources"); </script>
+</section>
+<?php
+include("footer.php");
+?>
