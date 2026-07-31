d9719adc50f0014046ffacf2828338d611a7fc13
Rose Enos <enosb@uci.edu>
2026-07-30 21:56:47 -0700

    Add CER researchers functionality

--- api/resources.php
+++ api/resources.php
@@ -3,6 +3,7 @@ $title = 'Resources';
 include('header.php');
 ?>
 <h1>Resources</h1>
+<p>Internal and external resources about computing, education, and computing education.</p>
 <h2>Rosa's Notas</h2>
 <p>I read a lot of textbooks, and I take notes on them. These are meant to be refreshers or references, not full notes, so you won't find many explanations, proofs, or practice problems. Sources and upcoming notes are (soon to be) on my <?php mention("https://github.com/LegoIready","GitHub"); ?>.</p>
 <p>Mathematics:</p>
@@ -18,10 +19,15 @@ include('header.php');
     <li><a href="/files/notes/Rose's Programming in Python.pdf" target="_blank">"Rose's Programming in Python."</a> 2024.</li>
     <li><a href="/files/notes/Rose's Programming in C++.pdf" target="_blank">"Rose's Programming in C++."</a> 2024.</li>
 </ul>
-<h2>External Resources</h2>
-<p>These are some helpful resources written by other people.</p>
 <section id="resources">
+<h2>External Resources</h2>
+<p>These are some helpful resources written by other people, mostly about computing, computing education, and PhD applications.</p>
     <script> echo_list("resources"); </script>
+</section>
+<section id="researchers">
+<h2>Computing Education Researchers</h2>
+<p>This is a partial list of computing education researchers that extends Amy J. Ko's <?php mention('https://faculty.washington.edu/ajko/cer','Computing Education Research FAQ'); ?>.</p>
+    <script> echo_list("researchers", "resources"); </script>
 </section>
 <?php
 include("footer.php");
